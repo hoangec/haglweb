19b91209e764efe4319d3f055b4cd9641df3929e
hoangec <vohoangec@gmail.com>
2024-04-10 13:58:40 +0700

    fix page lich su phat trien

--- themes/prismify-bootstrap-starter-kit/assets/js/main.js
+++ themes/prismify-bootstrap-starter-kit/assets/js/main.js
@@ -110,7 +110,33 @@
         }
     });
 
-
+    $(".nhan-su-carousel").owlCarousel({
+      autoplay: false,
+      smartSpeed: 1000,
+      margin: 25,
+      loop: true,
+      center: false,
+      dots: false,
+      nav: true,
+      navText : [
+          '<i class="bi bi-chevron-left"></i>',
+          '<i class="bi bi-chevron-right"></i>'
+      ],
+      responsive: {
+        0:{
+                  items:1
+              },
+              576:{
+                  items:1
+              },
+              768:{
+                  items:2
+              },
+              992:{
+                  items:3
+              }
+      }
+    });
     // Testimonial carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
